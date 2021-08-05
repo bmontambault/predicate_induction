e75b119fbf82657801e611eaf5c9f0acbef95fd5
bmontambault <brianmontambault@gmail.com>
2021-08-05 17:15:11 -0400

    added conditionally accepted

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='predicate_induction',
-    version='0.1.3',
+    version='0.1.4',
     packages=['predicate_induction'],
     install_requires=[
     ],
